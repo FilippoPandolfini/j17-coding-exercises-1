0d251cc4b2320cbd507c044646e9570acfd2c3b6
Filippo Pandolfini <pandolfini.filippo@gmail.com>
2025-05-07 11:54:48 +0200

    Esercizi

---
+++ Main.java
@@ -0,0 +1,139 @@
+import javax.swing.*;
+import javax.xml.namespace.QName;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void basicFilteringAndMapping() {
+
+        List<Integer> ints = List.of(10, 9, 20, 15, 92, 105, 1);
+        System.out.println("Lista completa: " + ints);
+
+        List<Integer> filtroDispari = ints.stream().filter(i -> i%2 !=0).toList();
+        System.out.println("Numeri dispari filtrati: " + filtroDispari);
+
+        ints.stream().filter(i -> i%2 !=0).forEach(System.out::println);
+
+        int prodottoRimanenti = ints.stream()
+                .filter(Main::filtraIntero)
+                .map(i -> -i)
+                .reduce(1, (a, b) -> a * b);
+        System.out.println(prodottoRimanenti);
+    }
+
+    public static boolean filtraIntero(int n){
+        return n%2==0;
+    }
+
+    public static void summmationAndReduction() {
+        List<Integer> ints = List.of(10, 9, 20, 15, 92, 105, 1);
+
+        int somma = ints.stream()
+                .mapToInt(i -> i)
+                .sum();
+
+        int differenza = ints.stream()
+                .mapToInt(i -> i)
+                .reduce(0, (a, b) -> a - b);
+
+        int differenzaDue = ints.stream()
+                .mapToInt(i -> i)
+                .reduce((a, b) -> a - b)
+                .getAsInt();
+
+        int differenzaConFiltro = ints.stream()
+                .mapToInt(i -> i)
+                .filter(i -> i%2 != 0)
+                .reduce(0, (a, b) -> a - b);
+
+        System.out.println(somma);
+        System.out.println(differenza);
+        System.out.println(differenzaDue);
+        System.out.println(differenzaConFiltro);
+    }
+
+    public static void collectingIntoDifferentDataStructure() {
+
+        List<String> listaConsole = new ArrayList<>();
+
+        listaConsole.add("PlayStation 4");
+        listaConsole.add("Nintendo Switch 2");
+        listaConsole.add("Steam Deck");
+        listaConsole.add("PlayStation 5");
+        listaConsole.add("XBox 360");
+        listaConsole.add("PlayStation 3");
+        listaConsole.add("Nintendo 3DS");
+        listaConsole.add("XBox One");
+        listaConsole.add("Nintendo Switch");
+
+        SortedSet<String> listaP = listaConsole.stream()
+                .filter(p -> p.contains("P"))
+                .collect(Collectors.toCollection(TreeSet::new));
+
+        Set<Character> inizialiConsentite = Set.of('N', 'X');
+
+        Map<Character, List<String>> listaFiltroIniziale = listaConsole.stream()
+                .filter(s -> !s.isEmpty() && inizialiConsentite.contains(s.charAt(0)))
+                .collect(Collectors.groupingBy(s ->
+                        s.charAt(0),
+                        TreeMap::new,
+                        Collectors.collectingAndThen(Collectors.toList(), list -> {
+                            list.sort(Comparator.naturalOrder());
+                            return list;
+                        }))
+                );
+
+        List<String> listaLunghe = listaConsole.stream()
+                .filter(s -> s.length() >= 15)
+                .toList();
+
+        System.out.println(listaP);
+        System.out.println(listaFiltroIniziale);
+        System.out.println(listaLunghe);
+    }
+
+    public static void groupingAndPartioning() {
+
+        List<Employee> employees = new ArrayList<>();
+
+        employees.add(new Employee("Pippo", "HR", 21000));
+        employees.add(new Employee("Pluto", "Finance", 33000));
+        employees.add(new Employee("Topolino", "Stagist", 12000));
+        employees.add(new Employee("Paperino", "CEO", 253000));
+        employees.add(new Employee("Paperone", "Finance", 50000));
+
+        Map<String, List<Employee>> employeeByDepartment = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment));
+
+        employeeByDepartment.forEach((dept, avg) -> System.out.println(dept + " " + avg));
+
+        System.out.println();
+
+        Map<Boolean, List<Employee>> paritioned = employees.stream().collect(Collectors.groupingBy(e -> e.getSalary() < 40000));
+
+        System.out.println("Dipendenti con RAL < 40000: ");
+        paritioned.get(true).forEach(System.out::println);
+    }
+
+    public static void flatMapNested() {
+        List<Integer> numeriPrimi = Arrays.asList(5, 7, 11, 13);
+        List<Integer> numeriDispari = Arrays.asList(1, 3, 5);
+        List<Integer> numeriPari = Arrays.asList(2, 4, 6, 8);
+
+        List<List<Integer>> listaDelleListe = Arrays.asList(numeriPrimi, numeriDispari, numeriPari);
+        System.out.println("Lista prima di flat: " + listaDelleListe);
+
+        List<Integer> listaFlat = listaDelleListe.stream().flatMap(Collection::stream).sorted().toList();
+        System.out.println("Lista flattata: " + listaFlat);
+    }
+
+    public static void main(String[] args) {
+        // basicFilteringAndMapping();
+        // summmationAndReduction();
+        // collectingIntoDifferentDataStructure();
+        // groupingAndPartioning();
+        flatMapNested();
+    }
+
+}
